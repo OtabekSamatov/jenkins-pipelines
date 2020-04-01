3dcaca40d05a207b79ac0eb3b353d81dc31356cf
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-31 20:21:32 -0500

    updated

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -12,21 +12,27 @@ pipelineTriggers([pollSCM('* * * * *')])
 	stage("Pull Repo"){
 		git 'https://github.com/farrukh90/cool_website.git'
 }
-	stage("Install Prerequisites"){
-		sh """ 
-		sudo yum install httpd -y
-		sudo cp ir * /var/www/html
-		sudo systemctl start httpd 
-
+		stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.acirrustech.com                 sudo yum install httpd -y
+		
 		"""
 }
-	stage("Stage3"){
-		echo "hello"
+		
 }
-	stage("Stage4"){
-		echo "hello"
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
+		ssh centos@jenkins_worker1.acirrustech.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_worker1.acirrustech.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_worker1.acirrustech.com				   sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_worker1.acirrustech.com				   sudo chmod 777 /var/www/html/*
+		"""
 }
-	stage("Stage5"){
-		echo "hello"
+	stage("Restart web server"){
+		sh "ssh centos@jenkins_worker1.acirrustech.com       sudo systemctl restart httpd"
+}
+	stage("Send Notifications to Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
 }
