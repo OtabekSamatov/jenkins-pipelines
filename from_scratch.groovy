3352c43e5bc931688f2bfe47faa2559c087d1e11
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-04-02 14:42:20 -0500

    added choice

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,38 +1,46 @@
 node {
-properties([
-	// Below line sets "Discard Builds more than 5"
-properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
-
-	// Below line triggers this job every minute 
-
-pipelineTriggers([pollSCM('* * * * *')])
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')]),
+		parameters([choice(choices: [
+			'dev1.otabeks.com', 
+			'qa1.otabeks.com', 
+			'stage1.otabeks.com', 
+			'prod1.otabeks.com'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR')]), 
 		])
-        
 
+		// Pulls a repo from developer
 	stage("Pull Repo"){
-		git 'https://github.com/farrukh90/cool_website.git'
-}
-		stage("Install Prerequisites"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+		//Installs web server on different environment
+	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_worker1.otabeks.com                 sudo yum install httpd -y
-		
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
 		"""
-}
-		
-}
+	}
+		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@jenkins_worker1.otabeks.com:/tmp
-		ssh centos@jenkins_worker1.otabeks.com                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.otabeks.com                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.otabeks.com				   sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.otabeks.com				   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
 		"""
-}
+	}
+		//Restarts web server
 	stage("Restart web server"){
-		sh "ssh centos@jenkins_worker1.acirrustech.com       sudo systemctl restart httpd"
-}
-	stage("Send Notifications to Slack"){
+		sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+	}
+
+		//Sends a message to slack
+	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
 }
